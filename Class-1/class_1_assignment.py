12bd2f3741e7629f0783176605a385429062c1bb
rawfun-shovon <rawfunshovon2020@gmail.com>
2026-09-03 20:24:01 +0600

    Update class_1_assignment.py

--- Class-1/class_1_assignment.py
+++ Class-1/class_1_assignment.py
@@ -1,11 +1,18 @@
-#Remove '#' from comment """ to comment the section
+#Remove '#' from the (""") to comment the section
 #Bookmark """
 #""""
 
 #Build a greeting program - Today's Assignment
 name =input("Enter your name: ")
-age = int(input("Enter your age: "))
-#print("Hello ", name,'!')
+while True:
+    try:
+        age = int(input("Enter your age: "))            #except ZeroDivisionError:
+        if age < 0:
+            print("Your age cannot be negative! Please try again.")
+            continue
+        break
+    except ValueError:                                  #except Exception:
+        print("Please enter a numeric value.")
 #print("Your age next year will be",age+1)
 next_year_age = age + 1
 print(f'Hello {name}! Next year you will be {next_year_age}.')
@@ -31,7 +38,13 @@ print('''\t Ask the user for a temperature in Celsius,
 print("")
 #print(f'Hello {name}!')
 
-celsius = float(input("Enter your temperature in Celsius: "))
+while True:
+    try:
+        celsius = float(input("Enter your temperature in Celsius: "))
+        break # right numbers are provided.
+    except ValueError:
+        print("Something went wrong! Please enter a numeric value.")
+
 fahrenheit = ((9 * celsius) / 5)+32
 print(f"The temperature is {fahrenheit} in Fahrenheit")
 
@@ -54,8 +67,14 @@ print('''Ask for a product's price and quantity, then print the total.
 Add a 5% tax on top and print the final amount too. Use meaningful variable names in snake_case .''')
 print("")
 
-product = float(input("Enter your product's price: "))
-quantity = int(input("Enter your quantity: "))
+while True:
+    try:
+        product = float(input("Enter your product's price: "))
+        quantity = int(input("Enter your quantity: "))
+        break
+    except ValueError:
+        print("Something went wrong! Please enter a numeric value.")
+
 total = float(product * quantity)
 print(''''Product bill total:''', total)
 print("")
@@ -68,7 +87,6 @@ print("Total tax is $", tax)
 final_price = float(total + tax)
 print("")
 print("Final price is $", final_price)
-
 
 print("")
 input("Press Enter for next problem.")
