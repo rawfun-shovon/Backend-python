92b39e223ce15bfaee158c1bde50d28c3ee659e6
rawfun-shovon <rawfunshovon2020@gmail.com>
2026-09-04 06:54:18 +0600

    Add files via upload

---
+++ Class-2/python-c2-homework-1.py
@@ -0,0 +1,22 @@
+#Remove '#' from the (""") to comment the section
+#Bookmark """
+#"""
+
+#Solution - 1
+print('''1. First & last element
+From [10, 20, 30, 40, 50] , print the first and last elements (use indexing,
+including -1 ).''')
+print("")
+input_list1 = [ 10, 20, 30, 40, 50]
+print(input_list1)
+print(f"0th index element: {input_list1[0]}, last index element: {input_list1[-1]}")
+
+print("")
+input("Press Enter for next problem.")
+print("")
+
+"""
+#Bookmark """
+#"""
+
+
