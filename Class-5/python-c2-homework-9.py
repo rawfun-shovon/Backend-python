e8bebd42454984fe6b65ece1f19043aa6fb84baa
rawfun-shovon <rawfunshovon2020@gmail.com>
2026-09-18 15:59:08 +0600

    Update python-c2-homework-9.py

--- Class-5/python-c2-homework-9.py
+++ Class-5/python-c2-homework-9.py
@@ -9,6 +9,15 @@ user_input = {
 }
 
 if "email" in user_input:
-    print("email is present")
+    print("email is present:", user_input["email"])
 else:
     print("Email not found!")
+
+#2nd method
+user_input1 = {
+    "username": "admin",
+    "password": "1234",
+    #"email": "email@localhost.com"
+}
+
+print(user_input1.get("email", "Email not found!"))
